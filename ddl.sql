19671839987087cc70bbd84b2a0249c660a25013
Kauã Borgarelli <borgarellitavares@outlook.com>
2024-05-13 11:41:33 +0000

    Add a new database

--- ddl.sql
+++ ddl.sql
@@ -1,12 +1,24 @@
+drop schema if exists spring;
+
+drop user if exists 'user'@'localhost';
+
+create schema spring;
+
+create user 'user'@'localhost' identified by 'pass123';
+
+grant select, insert, delete, update on spring.* to user@'localhost';
+
+use spring;
+
 CREATE TABLE IF NOT EXISTS problems (
-    problem_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    problem_id INTEGER PRIMARY KEY auto_increment,
     problem_name TEXT,
-    code_problem TEXT NOT NULL UNIQUE,
+    code_problem TEXT NOT NULL,
     output INTEGER NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS caseTests (
-    case_test_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    case_test_id INTEGER PRIMARY KEY auto_increment,
     result INTEGER,
     code TEXT NOT NULL,
     problem_id INTEGER,
@@ -14,7 +26,7 @@ CREATE TABLE IF NOT EXISTS caseTests (
 );
 
 CREATE TABLE IF NOT EXISTS solution (
-    solution_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    solution_id INTEGER PRIMARY KEY auto_increment,
     author_name TEXT NOT NULL,
     file_name TEXT NOT NULL,
     status TEXT,
