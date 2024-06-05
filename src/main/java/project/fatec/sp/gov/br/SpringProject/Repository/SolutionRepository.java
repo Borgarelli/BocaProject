1e1aa377215c14e6de94280fdb961576ed85612b
Kauã Borgarelli <kaua.tavares@fatec.sp.gov.br>
2024-06-05 14:59:40 -0300

    Ident a query

--- src/main/java/project/fatec/sp/gov/br/SpringProject/Repository/SolutionRepository.java
+++ src/main/java/project/fatec/sp/gov/br/SpringProject/Repository/SolutionRepository.java
@@ -6,10 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import project.fatec.sp.gov.br.SpringProject.Domain.Solution;
 import java.util.List;
 
-
 public interface SolutionRepository extends JpaRepository <Solution,Long>{
 
-    @Query("SELECT s FROM Solution s LEFT JOIN s.problem p WHERE p.idProblem = :problemId ORDER BY ASC s.created_at ")
+    @Query("SELECT s FROM Solution s " +
+            "LEFT JOIN s.problem p " +
+            "WHERE p.idProblem = :problemId " +
+            "ORDER BY ASC s.created_at ")
     List<Solution> findByProblemId(Long problemId);
 
 }
